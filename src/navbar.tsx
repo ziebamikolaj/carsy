59b32ba8eebdaccfd120ff3b05e56ff131ac89c7
ziebamikolaj <ziebamikolaj03@gmail.com>
2023-11-27 00:33:16 +0100

    fix: navbar mobile fix

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -14,22 +14,22 @@ const Navbar: React.FC<{ className?: string }> = ({ className }) => {
    const toggleMenu = () => setMenuOpen(!menuOpen);
    return (
       <div className={className}>
-         <div className="my-4 flex items-center justify-center">
+         <div className="my-4 flex items-center justify-center pt-16 md:pt-0">
             <img
                src="/carsy-logo.webp"
                alt="Carsy logo"
-               className="h-18 w-72"
+               className="h-18 fixed left-5 top-0 mt-6 w-72 md:static "
             />
             <img
                src={menuIcon}
                alt="menu icon"
-               className="ml-8 mr-8 h-12 w-12 cursor-pointer md:hidden"
+               className="fixed right-0 top-0 ml-8 mr-8 mt-8 h-12 w-12 cursor-pointer md:hidden"
                onClick={toggleMenu}
             />
             <ul
-               className={`flex h-screen flex-col items-center justify-center gap-4 p-2  ${
+               className={`flex h-screen flex-col items-center justify-center gap-4 p-2 ${
                   menuOpen ? "flex" : "hidden"
-               } md:mr-6 md:flex md:h-full md:w-5/6 md:flex-row md:justify-end md:gap-0 md:p-0 md:text-xl`}
+               } md:mr-6 md:flex md:h-full md:w-5/6 md:flex-row md:flex-nowrap md:justify-end md:gap-0 md:p-0 md:text-xl`}
             >
                {menuItems.map((item, index) => (
                   <li key={index} className="md:ml-4">
