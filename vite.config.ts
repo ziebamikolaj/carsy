7724499a940b7ecf24019dd7fb6bcb4cf5656772
ziebamikolaj <ziebamikolaj03@gmail.com>
2024-01-08 01:57:44 +0100

    fix: vercel env variables fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
+   define: {
+      "process.env": process.env,
+      ENV_KEY: process.env.ENV_KEY,
+   },
 });
