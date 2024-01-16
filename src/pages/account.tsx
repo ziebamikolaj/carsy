78d097faee19f3c0192506cbd318dec8ee4caa48
ziebamikolaj <ziebamikolaj03@gmail.com>
2024-01-16 03:06:02 +0100

    fix

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,19 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { HashLink } from "react-router-hash-link";
 
 const Account = () => {
-   const { data: user } = useQuery({
+   const { data: user } = useQuery<string>({
       queryKey: ["authCheck"],
-      queryFn: async () => {
-         const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/check`, {
-            credentials: "include",
-         });
-         const resText = await res.text();
-         if (res.ok) {
-            return resText;
-         } else {
-            return false;
-         }
-      },
    });
 
    return (
@@ -26,7 +15,7 @@ const Account = () => {
          <div className="flex flex-col gap-4 text-center font-bold text-font-primary">
             <HashLink
                to="/set-appointment"
-               className="b-nav-bg rounded px-4 py-2 text-3xl text-font-primary transition duration-300 ease-in-out hover:opacity-50"
+               className="rounded bg-nav-bg px-4 py-2 text-3xl text-font-primary transition duration-300 ease-in-out hover:opacity-50"
             >
                Umów wizytę
             </HashLink>
